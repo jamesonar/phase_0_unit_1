4ef2e4723c43e08052272f98146b43706a11612e
jamesonar <jamesonar@gmail.com>
2014-05-10 14:40:10 -0400

    Update my_solution.js

--- week_3/2_code_combat/my_solution.js
+++ week_3/2_code_combat/my_solution.js
@@ -1,16 +1,130 @@
 //I worked on this challenged by myself
  
 // Rescue Mission
-// Grab the Mushroom
-//Drink Me
-//Taunt the Guards
-//It's a Trap
-//Break the Prision
-//Taunt
-//Cowardly Taunt
-//Commanding Followers
-//Mobile Artillery
+// This is your code controlling Tharin!
+//this.moveDown();
+//this.moveDown();
+//this.moveRight();
+//this.moveUp();
+//this.moveUp();
+//this.moveRight();
+//this.moveRight();
+//this.moveDown();
+//this.attackNearbyEnemy()
+// Edit it anywhere you like, add comments like these
+// Follow the same "this.action();" format
+// Code is case-sensitive.
 
+// Grab the Mushroom
+// Tharin might consider taking a detour after line 3.
+//this.moveUp();
+//this.moveRight()
+//this.moveLeft()
+//this.moveUp();
+//this.attackNearbyEnemy();
+
+//Drink Me
+//this.moveRight();
+//this.attackNearbyEnemy();
+//this.moveRight();
+//this.moveDown();
+//this.moveUp();
+//this.moveRight();
+//this.attackNearbyEnemy();
+
+//Taunt the Guards
+//this.moveRight();
+//this.bustDownDoor();
+// Delete the "//" in front of each line below.
+//this.moveRight();
+//this.say("Hey there!");
+//this.moveLeft();
+//this.moveLeft();
+//this.say("Attack!");
+//this.moveRight();
+//this.say("Follow me.");
+//this.moveRight();
+//this.moveRight();
+//this.moveUp();
+//this.moveUp();
+//this.moveRight();
+//this.say("hi");
+//this.moveDown();
+//this.moveRight();
+//this.moveUp();
+//this.moveRight();
+
+//It's a Trap
+// Taunt the Ogre and run for cover!
+//this.moveDown();
+//this.moveDown();
+//this.say("hi");
+//this.moveUp();
+//this.moveUp();
+//this.moveUp();
+
+//Break the Prision
+// Write this isFriend(name) spell to tell friend from foe.
+// Return true for friends' names, false for ogres' names.
+//if(name === "William")
+    //return true;
+//if(name === "Krogg")
+    //return false;  // <-- Start here; Krogg is not a friend!
+//if(name === "Brack")
+    //return false
+//if(name === "MARCUS")
+    //return false
+//return true;
+
+//Taunt
+//this.say("Hey!");
+// Lure the ogre right into your arrow trap
+// by saying a few more things at him.
+// Anything works!
+//this.say("you're ugly");
+//this.say("bet you can't come over here");
+//this.say("your mama's so ugly...");
+
+//Cowardly Taunt
+//this.moveXY(50, 16);
+//this.moveXY(63, 20);
+//this.moveXY(70, 10);  // This is a safe spot.
+//this.say("I can lure them in here.");
+// Run your soldier out where the ogres can hear you.
+//this.moveXY(50,35);
+// Say something!
+//this.say("hi");
+// Then run back behind the arrow towers for safety.
+//this.moveXY(70,10);
+// You can shift+click on the map to insert coordinates.
+
+//Commanding Followers
+//this.moveXY(49, 66);
+//this.moveXY(60, 63);
+//this.moveXY(75, 63);
+//this.say("Hail, friends!");
+// Saying anything with "follow" in it will get nearby soldiers
+// to follow you
+//this.say("follow me");
+//this.moveXY(66, 40);
+// Saying anything with "attack" will send nearby troops into
+// a battle mode.
+//this.say("attack");
+// Make sure Tharin is safe!
+
+
+//Mobile Artillery
+//this.moveXY(30, 26);  // Move into range
+//this.attackXY(46, 5);  // Shoot once in the middle of the ogres
+// If they survive a hit, ogres retaliate!
+// Larger ogres take more than one hit...
+// So, position and aim carefully.
+//this.moveXY(33, 38);
+//this.attackXY(49, 63);
+//this.attackXY(42, 52);
+//this.moveXY(46,38);
+//this.attackXY(69, 54);
+//this.attackXY(54, 44);
  
  
  
